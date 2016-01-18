f0937ad692158b7d0db7ee4a94ab778cde4dddb0
Tessa <Tessachi33@yahoo.com>
2016-01-18 15:29:06 -0800

    Add console object and import console

--- Prompter.java
+++ Prompter.java
@@ -1,6 +1,12 @@
+import java.io.console;
+
 public class Prompter{
   private Game mGame;
   public Prompter(Game game){
     mGame = game;
+  }
+
+  public boolean promptForGuess() {
+    Console console = System.console();
   }
 }
