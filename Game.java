3b59b7c979e236591df18bc871c4abd626c56db3
Tessa <Tessachi33@yahoo.com>
2016-01-18 15:07:16 -0800

    Add Yes and No answer variables to Game object

--- Game.java
+++ Game.java
@@ -1,7 +1,11 @@
 public class Game {
  private String mAnswer;
+ private String mYes;
+ private String mNo;
 
  public Game(String answer) {
    mAnswer = answer;
+   mYes = "";
+   mNo = "";
  }
 }
