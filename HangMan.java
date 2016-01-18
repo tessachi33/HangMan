31d7e16f8787defc3ee5c02a43c308de7d82f88a
Tessa <Tessachi33@yahoo.com>
2016-01-18 15:43:36 -0800

    Add logic to print to console if our chosen letter was correct or not,WIP

--- HangMan.java
+++ HangMan.java
@@ -1,7 +1,16 @@
+import java.io.Console;
+
 public class HangMan {
 
     public static void main(String[] args) {
       Game game = new Game("Hire me!");
+      Prompter Prompter = new Prompter(game);
+      boolean isYes = Prompter.promptForGuess();
+      if(isYes) {
+        System.out.println("woo, right letter");
+      }else{
+        System.out.println("try again");
+      }
     }
 
 
