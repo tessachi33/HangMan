6e47ffeb98face4f952d6b5c6f9e97c19b06308b
Tessa <Tessachi33@yahoo.com>
2016-01-18 16:21:51 -0800

    Remove test methods and replace with play method

--- HangMan.java
+++ HangMan.java
@@ -5,15 +5,6 @@ public class HangMan {
     public static void main(String[] args) {
       Game game = new Game("Hire me!");
       Prompter prompter = new Prompter(game);
-      prompter.displayProgress();
-      boolean isYes = prompter.promptForGuess();
-      if(isYes) {
-        System.out.println("woo, right letter");
-      }else{
-        System.out.println("try again");
-      }
-      prompter.displayProgress();
+      prompter.play();
     }
-
-
 }
