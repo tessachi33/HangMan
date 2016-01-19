1bfb641fee82d6d3fe3a2726af149e9bea16031d
Tessa <Tessachi33@yahoo.com>
2016-01-18 16:06:58 -0800

    Add displayProgress just to check that the loop is working

--- HangMan.java
+++ HangMan.java
@@ -4,13 +4,15 @@ public class HangMan {
 
     public static void main(String[] args) {
       Game game = new Game("Hire me!");
-      Prompter Prompter = new Prompter(game);
-      boolean isYes = Prompter.promptForGuess();
+      Prompter prompter = new Prompter(game);
+      prompter.displayProgress();
+      boolean isYes = prompter.promptForGuess();
       if(isYes) {
         System.out.println("woo, right letter");
       }else{
         System.out.println("try again");
       }
+      prompter.displayProgress();
     }
 
 
