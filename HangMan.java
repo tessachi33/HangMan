546d6574d198ac464641d1f8944b66d674723b84
Tessa <Tessachi33@yahoo.com>
2016-01-20 09:37:51 -0800

    Add punctuation change for easier guessing

--- HangMan.java
+++ HangMan.java
@@ -3,7 +3,7 @@ import java.io.Console;
 public class HangMan {
 
     public static void main(String[] args) {
-      Game game = new Game("Hire me!");
+      Game game = new Game("hire me!");
       Prompter prompter = new Prompter(game);
       prompter.play();
     }
