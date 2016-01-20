cc8ef177485b362b8dab8165d0622f13fcb9b44a
Tessa <Tessachi33@yahoo.com>
2016-01-20 10:55:12 -0800

    super secret ;)

--- HangMan.java
+++ HangMan.java
@@ -3,7 +3,7 @@ import java.io.Console;
 public class HangMan {
 
     public static void main(String[] args) {
-      Game game = new Game("hire me!");
+      Game game = new Game("hired");
       Prompter prompter = new Prompter(game);
       prompter.play();
     }
